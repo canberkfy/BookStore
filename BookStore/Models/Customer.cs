e751918c176e689ae7b6ce22dc5918c185f5b0ce
canberkfy <canberkvatansever1@gmail.com>
2025-04-19 23:36:30 +0300

    Homework2

---
+++ BookStore/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string FirstName { get; set; }
+
+        [Required, MaxLength(100)]
+        public string LastName { get; set; }
+
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        
+    }
+}
---
+++ BookStore/ViewComponents/CategoryMenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using BookStore.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.ViewComponents
+{
+    public class CategoryMenuViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+        public CategoryMenuViewComponent(ApplicationDbContext context)
+            => _context = context;
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = await _context.Categories
+                .OrderBy(c => c.Name)
+                .ToListAsync();
+            return View(categories);
+        }
+    }
+}
